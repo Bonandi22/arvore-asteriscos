a2ed9abf0081f7ca156284e646cf26392e56a801
Elker <106995556+Bonandi22@users.noreply.github.com>
2022-07-18 14:57:54 +0100

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,38 +2,55 @@
 
 
 
-namespace Arvore_Asteriscos
+namespace Arvore_asteriscos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int N;
-           
+            String rep;
+
             Console.WriteLine("Digite o numero de linhas para a arvore");
             N = Convert.ToInt32(Console.ReadLine());
 
-
-            for (int i = 0; i <= N; i++)
+            do
             {
-                for (int j = N; j > i; j--)
+
+                for (int i = 0; i <= N; i++)
                 {
-                    Console.Write(" ");
+
+                    for (int j = N; j >= i; j--)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
                 }
-                for (int j = 0; j <= i; j++)
+
+                Console.WriteLine("Deseja alterar o tamando da arvore? S ou N");
+                rep = Console.ReadLine();
+
+                if (rep == "S" || rep == "s")
                 {
-                    Console.Write("*");
+
+                    Console.WriteLine("Digite o novo tamando da arvore");
+                    N = Convert.ToInt32(Console.ReadLine());
                 }
-                for (int j = 0; j < i; j++)
-                {
-                    Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+
+            } while (rep == "S" || rep == "s");
 
 
-        }               
-        
+        }
 
     }
 }
