fa1e31eb2cde4736781ebb39eecbc7b9bc0af1b2
Bonandi <elkerbonandi@gmail.com>
2022-07-18 14:51:53 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+namespace Arvore_Asteriscos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+           
+            Console.WriteLine("Digite o numero de linhas para a arvore");
+            N = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = N; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+        }               
+        
+
+    }
+}
